dcb9681644abf4b05d02adea0d0c900526c16e5b
Jan Klopper <jan@underdark.nl>
2021-03-12 14:43:11 +0100

    Inital commit of Python version of pUzi reader module

---
+++ README.md
@@ -0,0 +1,56 @@
+# pUZI python
+Proficient UZI pass reader in python, based on work by Anne Jan: https://github.com/minvws/pUZI-php
+
+The UZI card is part of an authentication mechanism for medical staff and doctors working in the Netherlands. The cards are distributed by the CIBG. More information and the relevant client software can be found at www.uziregister.nl (in Dutch).
+
+pUZI is a simple and functional module which allows you to use the UZI cards as authentication mechanism. It consists of:
+
+1. a reader that reads the data on the card and gives an UziUser object in return (this repository).
+
+pUZI is available under the EU PL licence. It was created early 2021 during the COVID19 campaign as part of the vaccination registration project BRBA for the ‘Ministerie van Volksgezondheid, Welzijn & Sport, programma Realisatie Digitale Ondersteuning.’
+
+Questions and contributions are welcome via [GitHub](https://github.com/minvws/pUzi-python/issues).
+
+## Requirements
+
+* python >= 3.6
+
+Apache config (or NginX equivalent):
+```apacheconf
+SSLEngine on
+SSLProtocol -all +TLSv1.3
+SSLHonorCipherOrder on
+SSLCipherSuite ECDHE-RSA-AES128-GCM-SHA256:ECDHE-RSA-AES256-GCM-SHA384:ECDHE-RSA-CHACHA20-POLY1305:DHE-RSA-AES128-GCM-SHA256:DHE-RSA-AES256-GCM-SHA384
+SSLVerifyClient require
+SSLVerifyDepth 3
+SSLCACertificateFile /path/to/uziCA.crt
+SSLOptions +StdEnvVars +ExportCertData
+```
+
+## Usage
+
+```python3
+import uzireader
+uzipas = uzireader.UziPassUser(env['SSL_CLIENT_VERIFY'], env['SSL_CLIENT_CERT'])
+print(uzipas)
+```
+
+```text
+{'givenName': 'john', 'surName': 'doe-11111111', 'OidCa': '2.16.528.1.1003.1.3.5.5.2', 'UziVersion': '1', 'UziNumber': '11111111', 'CardType': 'N', 'SubscriberNumber': '90000111', 'Role': '01.015', 'AgbCode': '00000000'}
+```
+
+## Uses
+
+[Python cryptography Library](https://cryptography.io/en/)
+
+## Contributing
+
+1. Fork the Project
+
+2. Ensure you have Black installed (pip3 install black)
+
+3. Create a Feature Branch
+
+4. (Recommended) Check whether your code conforms to our Coding Standards by running black on your changed files
+
+5. Send us a Pull Request
---
+++ tests/test_uzireader.py
@@ -0,0 +1,81 @@
+#!/usr/bin/python3
+__author__ = "Rafael Dulfer <rafael.dulfer@gmail.com>"
+
+import os
+import unittest
+
+import uzireader
+
+class TestUziReader(unittest.TestCase):
+    def setUp(self):
+        self.succ = "SUCCESS"
+        self.dir = os.path.dirname(__file__)
+
+    def readCert(self, path):
+        f = open(self.dir + "/certs/" + path, "r")
+        cert = f.read()
+        f.close()
+        return cert
+
+    def checkCert(self, path, message=None):
+        cert = self.readCert(path)
+        with self.assertRaises(uzireader.UziException, msg=message):
+            uzireader.UziPassUser(self.succ, cert)
+
+    def test_check_request_has_no_cert(self):
+        with self.assertRaises(uzireader.UziExceptionServerConfigError):
+            uzireader.UziPassUser()
+
+    def test_check_ssl_client_failed(self):
+        with self.assertRaises(uzireader.UziExceptionServerConfigError):
+            uzireader.UziPassUser()
+
+    def test_check_no_client_cert(self):
+        with self.assertRaises(uzireader.UziExceptionClientCertError):
+            uzireader.UziPassUser(self.succ)
+
+    def test_check_cert_without_valid_data(self):
+        self.checkCert("mock-001-no-valid-uzi-data.cert", "No valid UZI data found")
+
+    def test_check_cert_with_invalid_san(self):
+        self.checkCert("mock-002-invalid-san.cert", "No valid UZI data found")
+
+    def test_check_cert_with_invalid_other_name(self):
+        self.checkCert("mock-003-invalid-othername.cert", "No valid UZI data found")
+
+    def test_check_cert_without_ia5_string(self):
+        self.checkCert("mock-004-othername-without-ia5string.cert")
+
+    def test_check_cert_incorrect_san_data(self):
+        self.checkCert("mock-005-incorrect-san-data.cert", "Incorrect SAN found")
+
+    def test_check_cert_incorrect_san_data_2(self):
+        self.checkCert("mock-006-incorrect-san-data.cert", "Incorrect SAN found")
+
+    def test_check_valid_cert(self):
+        cert = self.readCert("mock-011-correct.cert")
+        data = uzireader.UziPassUser(self.succ, cert)
+
+        self.assertEqual("00000000", data["AgbCode"])
+        self.assertEqual("N", data["CardType"])
+        self.assertEqual("john", data["givenName"])
+        self.assertEqual("2.16.528.1.1003.1.3.5.5.2", data["OidCa"])
+        self.assertEqual("30.015", data["Role"])
+        self.assertEqual("90000111", data["SubscriberNumber"])
+        self.assertEqual("doe-12345678", data["surName"])
+        self.assertEqual("12345678", data["UziNumber"])
+        self.assertEqual("1", data["UziVersion"])
+
+    def test_check_valid_admin_cert(self):
+        cert = self.readCert("mock-012-correct-admin.cert")
+        data = uzireader.UziPassUser(self.succ, cert)
+
+        self.assertEqual("00000000", data["AgbCode"])
+        self.assertEqual("N", data["CardType"])
+        self.assertEqual("john", data["givenName"])
+        self.assertEqual("2.16.528.1.1003.1.3.5.5.2", data["OidCa"])
+        self.assertEqual("01.015", data["Role"])
+        self.assertEqual("90000111", data["SubscriberNumber"])
+        self.assertEqual("doe-11111111", data["surName"])
+        self.assertEqual("11111111", data["UziNumber"])
+        self.assertEqual("1", data["UziVersion"])
