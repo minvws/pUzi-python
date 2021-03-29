9d01c5222a2258373c76af333053967d1fe63cbc
Rafael Dulfer <rafael.dulfer@gmail.com>
2021-03-29 10:31:44 +0200

    Add new validator exceptions

--- uzireader/exceptions.py
+++ uzireader/exceptions.py
@@ -11,3 +11,19 @@ class UziExceptionServerConfigError(UziException):
 
 class UziExceptionClientCertError(UziException):
     """The client did not present a certificate"""
+
+
+class UziAllowedRoleException(UziException):
+    """The client card role is not allowed"""
+
+
+class UziAllowedTypeException(UziException):
+    """The client card type is not allowed"""
+
+
+class UziVersionException(UziException):
+    """The client card version is wrong"""
+
+
+class UziCaException(UziException):
+    """The client CA is invalid"""
